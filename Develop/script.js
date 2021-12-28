c9fd5365636af915ca455dd3b6e3f3253de67533
Francis Bond <francisbond1996@gmail.com>
2021-12-28 14:41:57 -0600

    Added comments to my code to make it better easier to follow

--- Develop/script.js
+++ Develop/script.js
@@ -42,9 +42,14 @@ function generatePassword(){
     // return string (alert with password to the screen)
 //5. display password in text box- write in page
 
+
+
+// Asking for length of password
 var lengthChoice = prompt("Please choose a length between 8-128 characters.")
 console.log("Lenght " + lengthChoice)
+// confirm password length is within limits
 if (7 < lengthChoice && lengthChoice < 129) {
+  // Ask for characters wanted
   var lowerChoice = confirm("Do you want lower case letters in your password?")
   console.log("lowercase " + lowerChoice)
   var upperChoice = confirm("Do you want upper case letters in your password?")
@@ -54,7 +59,9 @@ if (7 < lengthChoice && lengthChoice < 129) {
   var spcChoice = confirm("Do you want special characters in your password?")
   console.log("special " + spcChoice)
 
+  // confirm that a character type was chosen
   if (lowerChoice || upperChoice || numChoice || spcChoice) {
+    // create arrays of possible characters
     var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     var spcChar = ["~","!","@","#","$","%","^","&","*","(",")","_",":",";"]
@@ -62,6 +69,7 @@ if (7 < lengthChoice && lengthChoice < 129) {
 
     var finalChoices = []
 
+    // combine chosen characters into a new array
     if (lowerChoice) {
       finalChoices = finalChoices.concat(lowerCase);
     }
@@ -80,21 +88,24 @@ if (7 < lengthChoice && lengthChoice < 129) {
     var passwordString = []
 
     for(i = 0; i<lengthChoice ;i++){
-
+      // choose random characters from array and make a new one
       var randomPick = finalChoices[Math.floor(Math.random() * finalChoicesLength)]
       var passwordString = passwordString.concat(randomPick);
     }
     console.log(passwordString)
+    // return new random array as a string
     return passwordString.join("");
 
   } else {
+    // if no characters chosen then start over
     alert("Need at least one character type.")
     generatePassword();
   }
 } else if (lengthChoice == null){
-
+  // if when prompted for length, cancel is pressed, stop program
 } else
   generatePassword();
+  // if length outside of parameters is chosen then start over
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
